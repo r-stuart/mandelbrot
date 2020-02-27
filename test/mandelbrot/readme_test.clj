8a9d7bf9e1dd59f26b33131eb3b469abfe6eb9ed
Ryan Stuart <ryan@ryanstu.art>
2020-02-27 01:43:47 +0000

    Fix readme test

--- test/mandelbrot/readme_test.clj
+++ test/mandelbrot/readme_test.clj
@@ -1,6 +1,8 @@
 (ns mandelbrot.readme-test
   (:require [clojure.test :refer :all]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [mandelbrot.core :refer :all]
+            [mandelbrot.colours :as colours]))
 
 (deftest readme-exists
   (testing "Readme file is present"
@@ -10,7 +12,6 @@
   (testing "Readme examples successfully run"
     (let [example-image "docs/example.png"]
       (io/delete-file example-image)
-      (let [examples [(take-nth 2 (rest (clojure.string/split (slurp "README.md") #"```")))]]
-        (map eval (map read-string examples)))
+      (doall (map eval (map read-string (take-nth 2 (rest (clojure.string/split (slurp "README.md") #"```"))))))
       (is (.exists (io/file example-image))))
     ))
